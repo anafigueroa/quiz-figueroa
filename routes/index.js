ffc3421236151187f7d0a2a6e081b5523c6cb144
anafigueroa <alcyone@gmail.com>
2015-07-24 22:43:40 +0200

    Validación de entradas

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload: quizId
 
 router.get('/author', function(req, res){
-  res.render('author', { title: 'Créditos' });
+  res.render('author', { title: 'Créditos', errors: []});
 });
 
 // Definición de rutas de /quizes
